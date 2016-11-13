5e9242c8db2dd8f5400ebc1166ce80281580e35f
lev1917 <lev.ryskin@mail.ru>
2016-11-13 19:44:16 +0300

    Create close_hech_table.cpp

---
+++ close_hech_table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include<cmath>
+#include "close_hech_table.hpp"
+int my_hesh(std::string key){
+int hach=2139062143;
+for(int i-0;i<std::strlen(key);i++){
+hash=37*hash +int()key.substr(0, i);}
+return hash;}
+hech_table* building(int size){
+Node* Main=new Node[size];
+Hesh_tab*H=new Hesh_tab;
+H->Main_List=Main;
+H->size=size;
+return;}
