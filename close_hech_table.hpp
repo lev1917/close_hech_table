095972b2e7ca5a6de7748992c34cfc4d683d8eb9
lev1917 <lev.ryskin@mail.ru>
2016-11-13 19:44:37 +0300

    Update close_hech_table.hpp

--- close_hech_table.hpp
+++ close_hech_table.hpp
@@ -5,9 +5,9 @@ int my_hech(std::string L);
 std::string key;
 int value;}
 struct Hesh_table{
-Node** Main_List;
+Node* Main_List;
 int size;
-int size_;}
+}
 hech_table* building(int size);
 void add(hech_table &H,const Node &N);
 Node find(hec_table &H, std::string L);
